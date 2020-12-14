c9bdf631f79cf65b4165e5db4af448209dbc312d
chevtong <chevtongwingyan@gmail.com>
2020-12-14 23:15:51 +0100

    Build basic structure for landing page

---
+++ landing-page/script-index.js
@@ -0,0 +1,38 @@
+// set up text to print, each item in array is new line
+var aText = new Array(
+    "Zoom calls and game", 
+    );
+    var iSpeed = 200; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.querySelector(".typing");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos);
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 3000);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+
+    }
+    
+    
+    typewriter();
