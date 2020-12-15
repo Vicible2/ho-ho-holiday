3d11649879411515f97908fea2c5afb3fa803825
Manten Devillé <manten.d@hotmail.com>
2020-12-15 14:23:03 +0100

    Add basic variables for Js

---
+++ running-game/JS/running-app.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById('gameContainer');
+const context = canvas.getContext('2d');
+
+//Variables
+let score;
+let highscore;
+let player;
+let gravity;
+let obstacles;
+let gameSpeed;
+let keys = [];
+
+
+
