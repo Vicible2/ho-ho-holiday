3d1bb6e22731ca60a29be0ef33f925ede02973a8
chevtong <chevtongwingyan@gmail.com>
2020-12-15 14:03:11 +0100

    Adjust speed for typing effect on js/landingpage

--- landing-page/script-index.js
+++ landing-page/script-index.js
@@ -1,67 +1,56 @@
-//TODO: change the js for typewriting function
 
-// set up text to print, each item in array is new line
-var aText = new Array(
-    "Zoom calls and game", 
-    );
-    var iSpeed = 200; // time delay of print out
-    var iIndex = 0; // start printing array at this posision
-    var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
-     
-    var iTextPos = 0; // initialise text position
-    var sContents = ''; // initialise contents variable
-    var iRow; // initialise current row
-     
-    function typewriter()
-    {
-     sContents =  ' ';
-     iRow = Math.max(0, iIndex-iScrollAt);
-     var destination = document.querySelector(".typing");
-     
-     while ( iRow < iIndex ) {
-      sContents += aText[iRow++] + '<br />';
-     }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos);
-     if ( iTextPos++ == iArrLength ) {
-      iTextPos = 0;
-      iIndex++;
-      if ( iIndex != aText.length ) {
-       iArrLength = aText[iIndex].length;
-       setTimeout("typewriter()", 3000);
-      }
-     } else {
-      setTimeout("typewriter()", iSpeed);
-     }
+//typing effect function 
+ let typing = document.querySelector(".typing");
+ let text = typing.innerHTML;
 
+ typing.innerHTML = "";
+
+ //IF PUT IT I=0 INSIDE if ,THE FIRST LETTER WILL BE MISSING
+ let i = 0;
+
+typingEffect = () => {
+        
+    if (i < text.length){
+
+         //PRINT OUT THE FIRST LETTER
+         typing.innerHTML += text.charAt(i);
+
+         //THEN i++, IF NOT, FIRST LETTER WILL BE MISSING
+         i++;
+
+         //IF SPEED VARIABLE IS SET OUTSIDE THE FUNCTION, 
+         //IT WILL JUST GET ONE RANDOM NUM EACH TIME TO RUN THE FUNCTION
+         let delay = 400;
+
+         setTimeout(typingEffect, delay);
+     }
+ };
+ typingEffect();
+
+    
+//function for the snowflake
+getRandom = (min, max) => {
+    return Math.random() * (max - min) + min;
+  }
+  
+  (letItSnow = () => {
+    var snowflakes = document.querySelectorAll('.snow');
+    for (var i = 0; i < snowflakes.length; i++) {
+      var snowflake = snowflakes[i];
+      snowflake.setAttribute('cx', getRandom(1,100) + '%');
+      snowflake.setAttribute('cy', '-' + getRandom(1,100));
+      snowflake.setAttribute('r', getRandom(1,3));
     }
-    
-    
-    typewriter();
+  })();
 
-    
-
-    function getRandom(min, max) {
-        return Math.random() * (max - min) + min;
-      }
-      
-      (function letItSnow(){
-        var snowflakes = document.querySelectorAll('.snow');
-        for (var i = 0; i < snowflakes.length; i++) {
-          var snowflake = snowflakes[i];
-          snowflake.setAttribute('cx', getRandom(1,100) + '%');
-          snowflake.setAttribute('cy', '-' + getRandom(1,100));
-          snowflake.setAttribute('r', getRandom(1,3));
-        }
-      })();
-
+//function for text appear
 appearText = () =>{
 
   let text = document.querySelector(".game-text");
   let text2 = document.querySelector(".game-text-two");
   let text3 = document.querySelector(".game-text-three");
   let text4 = document.querySelector(".game-text-four");
-
+  
   let textLocation = text.getBoundingClientRect().top;
   let text2Location = text2.getBoundingClientRect().top;
   let text3Location = text3.getBoundingClientRect().top;
@@ -81,9 +70,6 @@ appearText = () =>{
   if (text4Location < screenPosition){
     text4.classList.add("game-text-four-appear");
   }
-
-
-  
-};
-window.addEventListener("scroll", appearText);
+}
+appearText();
 
