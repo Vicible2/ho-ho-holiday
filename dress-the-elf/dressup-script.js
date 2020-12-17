8b6bfd2ca7200194f7d6ce989dda72d4c4c1bd24
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2020-12-17 14:18:16 +0100

    add styling and basic mechanics of drag and drop

---
+++ dress-the-elf/dressup-script.js
@@ -0,0 +1,22 @@
+//TODO: limit dropzone
+
+interact(".draggable").draggable({
+    listeners: {
+        start (event) {
+            console.log(event.type, event.target)
+        },
+        move (event) {
+            let target = event.target;
+            let x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+            let y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            target.style.transform = `translate(${x}px, ${y}px)`;
+
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        },
+    }
+})
+
+
+
