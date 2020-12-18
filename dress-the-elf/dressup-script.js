908822b3a3038dca294cf21b8068d13f34d3c143
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2020-12-18 10:49:39 +0100

    add og tags and basic responsiveness

--- dress-the-elf/dressup-script.js
+++ dress-the-elf/dressup-script.js
@@ -1,6 +1,6 @@
 //TODO: add snap to drag elements
 
-    interact(".draggable").draggable({
+(interact(".draggable").draggable({
         listeners: {
             start (event) {
                 console.log(event.type, event.target)
@@ -23,10 +23,32 @@
                 endOnly: true
             })
         ]
-    })
+    }))();
 
+    (ShareSite = () => {
 
+        let postUrl = encodeURI(document.location.href);
+        let postTitle = encodeURI("Some Fun For the Holiday?");
 
+        facebookBtn.setAttribute(
+            "href",
+            `https://www.facebook.com/sharer.php?u=${postUrl}`
+        );
 
+        twitterBtn.setAttribute(
+            "href",
+            `https://twitter.com/share?url=${postUrl}&text=${postTitle}`
+        );
+
+        emailBtn.setAttribute(
+            "href",
+            `mailto:?subject=I wanted you to see this site&amp;body=${postTitle} ${postUrl}`
+        );
+
+        whatsappBtn.setAttribute(
+            "href",
+            `https://wa.me/?text=${postTitle} ${postUrl}`
+        );
+    })();
 
 
